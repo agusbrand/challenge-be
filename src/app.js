6b08fb6ac51e6672528711ee31ee181f3618d1dc
Agustin Brandoni <agusbrand@gmail.com>
2024-05-11 14:23:09 -0300

    feat: Authenticate all routes using the getProfile middleware

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize } = require('./models')
-const { getProfile } = require('./middleware/getProfile')
+const indexRouter = require('./routes')
+
 const app = express()
 app.use(bodyParser.json())
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id', getProfile, async (req, res) => {
-  const { Contract } = req.app.get('models')
-  const { id } = req.params
-  const contract = await Contract.findOne({ where: { id } })
-  if (!contract) return res.status(404).end()
-  res.json(contract)
-})
+app.use(indexRouter)
+
 module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,12 @@
 const express = require('express')
 const router = express.Router()
 
+const { getProfile } = require('../middleware/getProfile')
+
 const contractsRouter = require('./contractsRoutes.js')
+
+// Authenticates all routes using getProfile middleware
+router.use(getProfile)
 
 router.use('/contracts', contractsRouter)
 
